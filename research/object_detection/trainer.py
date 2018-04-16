c056d0a5a210aa709ddd82d3581a9b7d6e0c197e
Jay Yip <jay@powerarena.com>
2018-04-16 15:39:00 +0800

    fix tf bug for python3

--- research/object_detection/trainer.py
+++ research/object_detection/trainer.py
@@ -329,7 +329,7 @@ def train(create_tensor_dict_fn, create_model_fn, train_config, master, task,
     # Save checkpoints regularly.
     keep_checkpoint_every_n_hours = train_config.keep_checkpoint_every_n_hours
     saver = tf.train.Saver(
-        keep_checkpoint_every_n_hours=keep_checkpoint_every_n_hours)
+        keep_checkpoint_every_n_hours=keep_checkpoint_every_n_hours, max_to_keep=2)
 
     # Create ops required to initialize the model from a given checkpoint.
     init_fn = None
